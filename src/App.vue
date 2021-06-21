e0e343e51800c6f4eebd07e1f5c40638264f660b
DamianoTrifone <trifone.damiano@gmail.com>
2021-06-21 22:53:18 +0200

    Secondo blocco main completo

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script>
 import Header from './components/Header.vue';
-import Main from './components/Main.vue'
+import Main from './components/Main.vue';
 export default {
   name: 'App',
   components: {
@@ -22,6 +22,12 @@ export default {
 @import "~bootstrap/dist/css/bootstrap.css";
 @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500&display=swap');
 #app {
-  font-family: 'Noto Sans KR', sans-serif;
+  *{
+    font-family: 'Noto Sans KR', sans-serif;
+    box-sizing: border-box;
+    margin: 0 auto;
+    padding: 0;
+  }
+ 
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,7 +6,7 @@
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis voluptate ipsa veniam deleniti magnam tempore laborum, itaque architecto, officia veritatis exercitationem dignissimos.</p>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat aliquid cumque dolore quidem impedit architecto reprehenderit, necessitatibus ipsa.</p>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cumque consequuntur ipsum accusamus!</p>
-            <a href="#" id="mission_link">Our mission</a>
+            <a href="#" class="mission_link">Our mission</a>
         </div>
         <div class="col-lg-5 testo_dx">
             <div class="dati">
@@ -24,6 +24,42 @@
                 <h4>Donated</h4>
             </div>
         </div>
+        <div class="value">
+            <div class="value_text">
+                <h2>Our values</h2>
+                <!-- <div id="separatore_value"></div> -->
+                <p>Think Big. Act Bigger.</p>
+            </div>  
+            <div class="cards">
+                <div>
+                    <i class="fas fa-tint"></i>
+                    <h4>Clean Water</h4>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est eligendi sint adipisci molestiae assumenda.</p>
+                    <a href="#" class="mission_link">Learn more</a>
+                </div>
+
+                <div>
+                    <i class="fas fa-stethoscope"></i>
+                    <h4>Vaccinations</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem omnis dolore dolores hic quas.</p>
+                    <a href="#" class="mission_link">Learn more</a>
+                </div>
+
+                <div>
+                    <i class="fas fa-graduation-cap"></i>
+                    <h4>Education</h4>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est reprehenderit facilis architecto</p>
+                    <a href="#" class="mission_link">Learn more</a>
+                </div>
+
+                <div>
+                    <i class="fas fa-tractor"></i>
+                    <h4>Farming</h4>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda perferendis quas dolores dicta fugiat.</p>
+                    <a href="#" class="mission_link">Learn more</a>
+                </div>
+            </div>  
+        </div>
     </main>
 </template>
 
@@ -34,9 +70,22 @@ export default {
 </script>
 
 <style>
+    .main{
+        position: relative;
+    }
     .testo_sx{
         float: left;
+        background-color: white;
+        height: 384px;
     }
+
+    .testo_sx::after,
+    .testo_dx::after{
+        content: "";
+        display: table;
+        clear: both;
+    }
+
     .testo_sx h3{
         margin-left: 30px;
         margin-top: 35px;
@@ -79,10 +128,10 @@ export default {
         padding: 20px;
     }
 
-    #mission_link{
+    .mission_link{
         text-decoration: none;
         border: 2px solid #FCD66C;
-        padding: 6px 12px;
+        padding: 6px 16px;
         color: white;
         background-color: #FCD66C;
         margin-left: 17px;
@@ -90,8 +139,60 @@ export default {
         font-size: 13px;
     }
 
-    #mission_link:hover{
+    .mission_link:hover{
         color: #FCD66C;
         background-color: white;
+    }
+
+    .value{
+        background-image: url(../assets/home-content-bg-2.jpg);
+        height: 620px;
+        width: 100%;
+        background-size: cover;
+        background-repeat: no-repeat;
+        position: absolute;
+        top: 1065px;
+        text-align: center;
+    }
+
+    .value_text{
+        margin-top: 150px;
+        color: white;
+    }
+
+    .value_text h2{
+        text-transform: uppercase;
+        font-weight: 200;
+    }
+
+    #separatore_value{
+        height: 1px;
+        width: 150px;
+        color: #FCD66C;
+        border: 4px solid #FCD66C;
+    }
+
+    .cards{
+        margin-top: 80px;
+        display: flex;
+        justify-content: space-evenly;
+        /* width: calc(90% / 4); */
+    }
+
+    .cards i{
+        color: #FCD66C;
+        font-size: 50px;
+    }
+
+    .cards h4,
+    .cards p{
+        color: white;
+        width: 290px;
+        font-weight: 400;
+        padding-top: 5px;
+    }
+
+    .cards a{
+
     }
 </style>
