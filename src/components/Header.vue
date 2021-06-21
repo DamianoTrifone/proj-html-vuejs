ed1b7910bc2a6e83af2b1d7188df0521bd15c3c1
DamianoTrifone <trifone.damiano@gmail.com>
2021-06-21 15:32:43 +0200

    Header completo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,6 +8,12 @@
           <a href="#">Journal</a>
           <a class="special_link" href="#">Donate</a>
       </div>
+      <div class="header_centrale">
+          <h2>Make a difference</h2>
+          <p>As long as poverty, injustice & inequality persist, none of us can truly rest</p>
+          <a href="#">Our mission</a>
+          <a href="#">Donate now</a>
+      </div>
   </header>
 </template>
 
@@ -22,7 +28,7 @@ export default {
         background-image: url(../assets/home-page-slider.jpg);
         background-size: cover;
         background-repeat: no-repeat;
-        height: 600px;
+        height: 680px;
         width: 100%;
     }
 
@@ -58,5 +64,46 @@ export default {
     .special_link:hover{
         color: white;
         background-color: white;
+    }
+
+    .header_centrale{
+        text-align: center;
+        margin-top: 14%;
+    }
+
+    .header_centrale h2{
+        text-transform: uppercase;
+        color: white;
+        font-weight: 300;
+        font-size: 60px;
+    }
+
+    .header_centrale p{
+        color: white;
+        font-weight: 200;
+        font-size: 20px;
+    }
+
+    .header_centrale a{
+        text-decoration: none;
+        text-transform: uppercase;
+        margin-left: 15px;
+        color: white;
+        border: 1px solid white;
+        padding: 4px 8px;
+        font-weight: 200;
+    }
+
+    .header_centrale a:hover{
+        color: #FCD66C;
+    }
+
+    .header_centrale a:last-child{
+        color: #FCD66C;
+        border: 1px solid #FCD66C;
+    }
+
+    .header_centrale a:last-child:hover{
+        color: white;
     }
 </style>
