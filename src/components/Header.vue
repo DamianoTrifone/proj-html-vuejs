430ccf81cb004092b8a528b8e643c96544dc6a0a
DamianoTrifone <trifone.damiano@gmail.com>
2021-06-21 12:06:13 +0200

    Import framework / Sfondo header

---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+
+  </header>
+</template>
+
+<script>
+export default {
+    name:"header"
+}
+</script>
+
+<style>
+    header{
+        background-image: url(../assets/home-page-slider.jpg);
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 600px;
+        width: 100%;
+    }
+</style>
